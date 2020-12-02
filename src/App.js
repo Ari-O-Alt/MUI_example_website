17bec550a7cb82d089e65ed4dd728db4452579c0
Silvia Bogdan <titifursec@outlook.com>
2020-12-02 21:48:04 +0100

    Test Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Button variant='contained' color='primary'>
+        Hello World
+      </Button>
+    </div>
+  );
 }
 
 export default App;
