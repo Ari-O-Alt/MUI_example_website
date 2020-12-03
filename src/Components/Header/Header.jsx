16355aada99b9fdec00713268edaba5455bba4f2
Silvia Bogdan <titifursec@outlook.com>
2020-12-03 14:35:18 +0100

    Added navigation buttons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { NavLink } from 'react-router-dom';
+import { Button, ButtonGroup } from '@material-ui/core';
+import './Header.css';
 
 const Header = () => {
   return (
-    <Grid container={true}>
+    <Grid container={true} className='headerContainer'>
       <Grid item={true} xs={6}></Grid>
-      <Grid container={true} item={true} xs={6}>
-        <Grid item={true} xs={4}>
-          <NavLink exact to='/' activeClassName='current'>
-            Home
-          </NavLink>
-        </Grid>
-        <Grid item={true} xs={4}>
-          <NavLink exact to='/blog' activeClassName='current'>
-            Blog
-          </NavLink>
-        </Grid>
-        <Grid item={true} xs={4}>
-          <NavLink exact to='/contact' activeClassName='current'>
-            Contact
-          </NavLink>
-        </Grid>
+      <Grid container={true} item={true} xs={6} className='navItemContainer'>
+        <ButtonGroup variant='text' color='primary' aria-label='text primary button group'>
+          <Button>
+            <NavLink exact to='/' activeClassName='current' className='navLink'>
+              Home
+            </NavLink>
+          </Button>
+          <Button>
+            <NavLink exact to='/blog' activeClassName='current' className='navLink'>
+              Blog
+            </NavLink>
+          </Button>
+          <Button>
+            <NavLink exact to='/contact' activeClassName='current' className='navLink'>
+              Contact
+            </NavLink>
+          </Button>
+        </ButtonGroup>
       </Grid>
     </Grid>
   );
